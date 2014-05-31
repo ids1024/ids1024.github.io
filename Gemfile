4141f888753b0ccc57bd0579085cfe2d6a6aec0b
Ian Scott <ian@perebruin.com>
2014-05-31 07:40:07 -0700

    Switch to middleman-deploy

--- Gemfile
+++ Gemfile
@@ -12,4 +12,4 @@ gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
 gem 'middleman-blog'
 
-gem 'middleman-gh-pages'
+gem 'middleman-deploy'
--- config.rb
+++ config.rb
@@ -74,3 +74,12 @@ activate :blog do |blog|
 end
 
 Time.zone = "America/Los_Angeles"
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true # default: false
+  # deploy.remote   = "custom-remote" # remote name or git url, default: origin
+  deploy.branch   = "master" # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = "custom-message"      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
